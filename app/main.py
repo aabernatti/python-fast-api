7790cdc391d81f952924e09ff874f1646a0d2500
zahidferz <zahid.ferz@gmail.com>
2025-07-14 11:05:58 -0600

    Bernatti fast-api Lint

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
+
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/index")
 
+
 @app.get("/index", response_class=HTMLResponse)
 async def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request}) 
+    return templates.TemplateResponse("index.html", {"request": request})
